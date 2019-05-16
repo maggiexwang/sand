8e2bc3b632cc1eca9c563ddb8fe9b2fd83e955c9
Maggie X. Wang <xwang73@smith.edu>
2019-05-16 10:19:03 +0200

    Update display adjustments

--- sand_page.js
+++ sand_page.js
@@ -1,5 +1,16 @@
 jQuery(function(){
   createSticky(jQuery("#stickyheader"));
+  // Added styles - 6 May
+  adjustDisplay(jQuery("#stickyheader"));
+  adjustDisplay(jQuery("#banner"));
+  adjustDisplay(jQuery("#bannerwrap"));
+  adjustDisplay(jQuery("#download_container"));
+  adjustDisplay(jQuery("#fullreport"));
+  adjustDisplay(jQuery("#summary"));
+  adjustDisplay(jQuery("#content"));
+  adjustDisplay(jQuery("#partnership"));
+  adjustDisplay(jQuery("#logo_ch"));
+  adjustDisplay(jQuery("#logo_unige"));
 });
 
 function createSticky(sticky) {
@@ -17,4 +28,16 @@ function createSticky(sticky) {
         /* Pleaes insert correct image server location above */
     }
   });
+}
+
+// Added styles - 6 May
+function adjustDisplay(element) {
+  var width = $(window).width();
+  var height = $(window).height();
+
+  if (width<=height*0.8) {
+    element.addClass("narrow");
+  } else {
+    element.removeClass("narrow");
+  }
 }
